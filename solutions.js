4b52f664275bdb808161926e34799128d5e4945f
Juliana Fernandes <julianafernandes@Albertos-MacBook-Pro.local>
2020-08-28 23:46:23 -0300

    finished loops deliverable

---
+++ solutions.js
@@ -0,0 +1,84 @@
+//GET EVEN
+for(let i = 0;i<200;i++){
+  if (i%2===0){
+    console.log(i)
+  }
+}
+
+//EXCITED KITTEN
+
+ var text = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "meow?",  "...why does the red dot always get away..."]
+
+for(let i = 0;i<=10;i++){
+ 
+    if (i%2===0){
+    // I searched online for how to search for a random item in an array:https://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
+    console.log(text[Math.floor(Math.random()*text.length)])
+ } 
+ console.log("Love me, pet me! HSSSSSS!")
+}
+
+//THERMOSTAT
+var currentTemp = Math.floor(Math.random() * 101)
+desiredTemp = 70+"F";
+
+while (currentTemp!==desiredTemp){
+  if(currentTemp<desiredTemp){
+    for (j = currentTemp;j<desiredTemp;i++){
+      console.log("The current temperature is",currentTemp+"F")
+    }
+    } else if(currentTemp>desiredTemp){
+     for (j = currentTemp;j>desiredTemp;i--){
+      console.log("The current temperature is",currentTemp+"F")
+    }
+  } 
+}
+
+
+
+//FIZZBUZZ
+for(let i = 0; i<=100;i++){
+  if(i%3===0){
+    console.log("Fizz")
+  }else if(i%5===0){
+    console.log("Buzz")
+  }if(i%3===0 && i%5===0){
+    console.log("FizzBuzz")
+  }
+  console.log(i)
+}
+
+//WHAT'S MY NUMBER
+var phoneBook = {
+  "Abe": "111-111-1111",
+  "Bob": "222-222-2222",
+  "Cam": "333-333-3333",
+  "Dan": "444-444-4444",
+  "Ern": "555-555-5555",
+  "Fry": "111-111-1111",
+  "Gil": "222-222-2222",
+  "Hal": "333-333-3333",
+  "Ike": "444-444-4444",
+  "Jim": "555-555-5555",
+  "Kip": "111-111-1111",
+  "Liv": "222-222-2222",
+  "Mia": "333-333-3333",
+  "Nik": "444-444-4444",
+  "Oli": "555-555-5555",
+  "Pam": "111-111-1111",
+  "Qiq": "222-222-2222",
+  "Rob": "333-333-3333",
+  "Stu": "444-444-4444",
+  "Tad": "555-555-5555",
+  "Uwe": "111-111-1111",
+  "Val": "222-222-2222",
+  "Wil": "333-333-3333",
+  "Xiu": "444-444-4444",
+  "Yam": "555-555-5555",
+  "Zed": "111-111-1111"
+}
+for(phone in phoneBook){
+  if (phoneBook[phone] === "333-333-3333"){
+    console.log(phone)
+  }
+}
